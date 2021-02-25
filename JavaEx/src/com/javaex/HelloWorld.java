1d69450302fd44e956cd079e728d4b44eb8701b3
이유승 <yousi7@naver.com>
2021-02-25 14:46:54 +0900

    Initial Commit

---
+++ JavaEx/src/com/javaex/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.javaex;
+
+// 주석 : 컴파일되지 않는 설명문
+public class HelloWorld {
+	/*
+	 * 여러줄 주석
+	 */
+//	객체가 실행되고자 한다면 main메서드가 필요
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+	}
+	
+}
