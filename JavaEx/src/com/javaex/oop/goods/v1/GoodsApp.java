9d726b0effef650547b98417cfcab179fa8b9348
이유승 <yousi7@naver.com>
2021-03-05 01:22:34 +0900

    객체 필드와 메서드

--- JavaEx/src/com/javaex/oop/goods/v1/GoodsApp.java
+++ JavaEx/src/com/javaex/oop/goods/v1/GoodsApp.java
@@ -18,5 +18,4 @@ public class GoodsApp {
 				smartphone.name, smartphone.price);
 
 	}
-
 }
--- JavaEx/src/com/javaex/oop/goods/v2/Goods.java
+++ JavaEx/src/com/javaex/oop/goods/v2/Goods.java
@@ -6,4 +6,20 @@ public class Goods {
 	//	public > protected > default > private
 	private String name;
 	private int price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public void showInfo() {
+		System.out.printf("상품 이름:%s%n가격: %d원%n", name, price);
+	}
 }
