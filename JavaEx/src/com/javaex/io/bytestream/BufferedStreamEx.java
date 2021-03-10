420580e2994054ec692d64be109dab69c311ee8f
이유승 <yousi7@naver.com>
2021-03-10 13:03:20 +0900

    보조 스트림

--- JavaEx/src/com/javaex/io/bytestream/BufferedStreamEx.java
+++ JavaEx/src/com/javaex/io/bytestream/BufferedStreamEx.java
@@ -10,7 +10,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class BufferedStreamEx {
-	static String rootPath = System.getProperty("user.dir") + "\\files\\";
+	static final String rootPath = System.getProperty("user.dir") + "\\files\\";
 	static final String source = rootPath + "img.jpg";
 	static final String target = rootPath + "img_buffered_copy.jpg";
 	
